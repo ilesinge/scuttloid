733edb5bad4ec50ff4fa705fa7625e7756e04a7f
Alexandre G.-Raymond <alex@ndre.gr>
2013-04-06 09:05:01 +0200

    Do not refresh list when returning from detail view

--- src/gr/ndre/scuttloid/BookmarkDetailActivity.java
+++ src/gr/ndre/scuttloid/BookmarkDetailActivity.java
@@ -1,8 +1,6 @@
 package gr.ndre.scuttloid;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
 
 /**
@@ -50,15 +48,7 @@ public class BookmarkDetailActivity extends ScuttloidActivity {
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch (item.getItemId()) {
 		case android.R.id.home:
-			// This ID represents the Home or Up button. In the case of this
-			// activity, the Up button is shown. Use NavUtils to allow users
-			// to navigate up one level in the application structure. For
-			// more details, see the Navigation pattern on Android Design:
-			//
-			// http://developer.android.com/design/patterns/navigation.html#up-vs-back
-			//
-			NavUtils.navigateUpTo(this, new Intent(this,
-					BookmarkListActivity.class));
+			finish();
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
